a5bff6b9bbc52958e278ae6ccecc1938941b5d67
deiyr1269-sudo <deiyr1269@gmail.com>
2026-08-25 09:31:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded',function () {
    document.getElementByid('year').innerHTML = new Date().getFullYear();
-   console.log('Portfolio ของ SOMCHAI พร้อมแล้ว!');
+   console.log('Portfolio ของ Pakjira พร้อมแล้ว!');
 }};
