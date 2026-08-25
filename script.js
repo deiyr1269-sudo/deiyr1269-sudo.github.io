3bbf6685f5d822290165efb61f60e8fdbd0baccd
deiyr1269-sudo <deiyr1269@gmail.com>
2026-08-25 09:28:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใล้ (Optional)
 document.addEventListener('DOMContentLoaded',function () {
-   
+   document.getElementByid('year').innerHTML = new Date().getFullYear();
    console.log('Portfolio ของ SOMCHAI พร้อมแล้ว!');
 }};
